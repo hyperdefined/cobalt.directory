152062ccbe182436f77edc70296d091e2bd69bf6
hyperdefined <contact@hyper.lol>
2025-10-07 21:42:34 -0400

    env not working for zero reason

--- web/src/routes/api/tests/+server.ts
+++ web/src/routes/api/tests/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import fs from 'node:fs';
 
 export const GET = async () => {
-  const filePath = process.env.TESTS_JSON_PATH ?? '/data/tests.json'; // default
+  const filePath = process.env.TESTS_JSON_PATH ?? '/data/results.json'; // default
   const raw = fs.readFileSync(filePath, 'utf-8');
   const stats = fs.statSync(filePath);
   const data = JSON.parse(raw);
