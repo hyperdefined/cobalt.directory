a350ac69bf183f7d93fb52e7abaa3020f9550c6f
hyperdefined <contact@hyper.lol>
2025-10-09 18:31:12 -0400

    remove unused libraries

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,16 +1,15 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
 			out: 'build'
 		})
 	},
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte']
 };
 
 export default config;
